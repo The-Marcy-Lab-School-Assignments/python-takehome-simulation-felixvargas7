00bcf82b9d3534647c376600fe58bc1d4e23d77e
Felix Vargas <felix70529@gmail.com>
2026-06-15 15:35:56 -0400

    Completed python-takehome-simulation part 1

---
+++ analysis.py
@@ -0,0 +1,62 @@
+# Import the csv package to access DictReader
+import csv
+
+rows = []
+
+with open('nyc_311_requests.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        rows.append(row)
+
+# example of what the first data row looks like
+# row = {
+#   'request_id': 1001,
+#   'date': '2024-01-03',
+#   'borough': 'Brooklyn',
+#   'complaint_type': 'Noise - Residential',
+#   'agency': 'NYPD',
+#   'resolution_status': 'Closed'
+# }
+
+# meaning_of_life = 42
+# f"Meaning of life: {meaning_of_life}\n
+
+counter = 0
+for row in rows:
+  if row['resolution_status'] == 'Open':
+    counter += 1
+
+
+complaint_types = {}
+for row in rows:
+  if (row['complaint_type'] not in complaint_types):
+    complaint_types[row['complaint_type']] = 0
+  complaint_types[row['complaint_type']] += 1
+
+max_complaint = None
+count = 0
+
+for key, value in complaint_types.items():
+  if value > count:
+    max_complaint = key
+    count = value
+
+
+borough_requests = {}
+for row in rows:
+  if (row['borough'] not in borough_requests):
+    borough_requests[row['borough']] = 0
+  borough_requests[row['borough']] += 1
+
+sorted_boroughs = sorted(borough_requests)
+
+with open('output.txt', 'w') as f:
+    f.write(f"Open requests: {counter}\n")
+    f.write(f"\n")
+    f.write(f"Most common complaint type: {max_complaint} ({count} requests)\n")
+    f.write("\n")
+    f.write(f"Request per borough: \n")
+    for borough in sorted_boroughs:
+       f.write(f"- {borough}: {borough_requests[borough]}\n")
+
+print("Output saved to output.txt")
